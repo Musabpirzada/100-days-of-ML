225153b4a4bc8500cf95e754aa238f0645cccd51
Musab <musabpirzada1@gmail.com>
2023-03-28 19:06:56 +0500

    compress the string

---
+++ Day_5/compress_the_string.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+inp = input()
+abc = []
+n = len(inp)
+i = 0
+while i < n:
+    j = i+1
+    while j < n and inp[j] == inp[i]:
+        j += 1
+    abc.append((j-i, int(inp[i])))
+    i = j
+print(" ".join(map(str, abc)))
